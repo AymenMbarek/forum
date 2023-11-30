325f9a452e0eb7b8a696e645799d5c7453d76235
aymen mbarek <aymen199719@hotmail.fr>
2023-12-01 00:41:51 +0100

    uploading application

---
+++ src/forum/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreBBS.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "nom d'utilisateur")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "mot de passe")]
+        public string Password { get; set; }
+
+        [Display(Name = "mémoriser le statut de connexion")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "nom d'utilisateur")]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(16, ErrorMessage = "{0} doit être supérieur à {2} bits", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmez le mot de passe")]
+        [Compare("Password", ErrorMessage = "Deux mots de passe sont incohérents")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
